e866e91ac97cdc5d6ec69073b125d6dd2679452a
Erik Ostlund <ostlu058@umn.edu>
2018-01-12 11:33:40 -0600

    Added List Item Component
    Will be used for meals and workouts.

---
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'list-item',
+    styleUrls: ['list-item.component.scss'],
+    templateUrl: 'list-item.component.html'
+})
+export class ListItemComponent {
+
+    @Input()
+    item: any; // because we will use in meals and workouts
+
+    constructor() {}
+}
--- src/health/shared/shared.module.ts
+++ src/health/shared/shared.module.ts
@@ -5,15 +5,28 @@ import { RouterModule } from '@angular/router';
 // third party
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+// components
+import { ListItemComponent } from './components/list-item/list-item.component';
+
+// services
 import { MealsService } from './services/meals/meals.service';
 
 @NgModule({
+    // register modules here
     imports: [
         CommonModule,
         RouterModule,
         AngularFireDatabaseModule
     ],
-    declarations: []
+    // register components here
+    declarations: [
+        ListItemComponent
+    ],
+    // if something needs to be used outside of this module, add it here
+    exports: [
+        // will be used in meals module and workouts module
+        ListItemComponent
+    ]
 })
 export class SharedModule {
     // to prevent duplicate instances of the sharedModule
